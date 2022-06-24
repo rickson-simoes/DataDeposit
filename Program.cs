02a3eb0cf099a805035e2b8332bd2a42e83ca1ba
Rickson Simoes <rickson.simoes@hotmail.com>
2022-06-24 15:30:46 -0300

    :wrench: ADO - SqlClient

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Data.SqlClient;
 
 namespace DataDeposit
 {
@@ -6,7 +7,28 @@ namespace DataDeposit
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      const string ConnectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=a1b2c3d4#@!;Trusted_Connection=False;TrustServerCertificate=True;";
+
+      using (var connection = new SqlConnection(ConnectionString)) 
+      {
+        Console.WriteLine("Conectado");
+        connection.Open();
+         Console.WriteLine("");
+
+        using(var command = new SqlCommand())
+        {
+          command.Connection = connection;
+          command.CommandType = System.Data.CommandType.Text;
+
+          command.CommandText = "SELECT ID, TITLE FROM CATEGORY";
+
+          var reader = command.ExecuteReader();
+          while(reader.Read())
+          {
+            Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+          }
+        }
+      }
     }
   }
 }
