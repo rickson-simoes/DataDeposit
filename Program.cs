a355bcad2d718942b73698d91c32b1d092d6d67e
Rickson Simoes <rickson.simoes@hotmail.com>
2022-06-25 14:13:47 -0300

    :wrench: DAO Removal

--- Program.cs
+++ Program.cs
@@ -8,25 +8,11 @@ namespace DataDeposit
     static void Main(string[] args)
     {
       const string ConnectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=a1b2c3d4#@!;Trusted_Connection=False;TrustServerCertificate=True;";
-
-      using (var connection = new SqlConnection(ConnectionString)) 
+            
+      using (var connection = new SqlConnection(ConnectionString))
       {
-        Console.WriteLine("Conectado");
-        connection.Open();
-         Console.WriteLine("");
-
-        using(var command = new SqlCommand())
         {
-          command.Connection = connection;
-          command.CommandType = System.Data.CommandType.Text;
-
-          command.CommandText = "SELECT ID, TITLE FROM CATEGORY";
-
-          var reader = command.ExecuteReader();
-          while(reader.Read())
-          {
-            Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-          }
+          
         }
       }
     }
