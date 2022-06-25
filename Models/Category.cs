07a7efbebb1c264141be8381f27b560881cde482
Rickson Simoes <rickson.simoes@hotmail.com>
2022-06-25 16:06:36 -0300

    Dapper - Primeira chamada ao banco

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataDeposit.Models
+{
+  public class Category
+  {
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Dapper;
+using DataDeposit.Models;
 using Microsoft.Data.SqlClient;
 
 namespace DataDeposit
@@ -8,13 +10,18 @@ namespace DataDeposit
     static void Main(string[] args)
     {
       const string ConnectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=a1b2c3d4#@!;Trusted_Connection=False;TrustServerCertificate=True;";
-            
+
       using (var connection = new SqlConnection(ConnectionString))
       {
+        var categories = connection.Query<Category>("SELECT * FROM CATEGORY");
+
+        foreach(var category in categories)
         {
-          
+          Console.WriteLine($"{category.Id} - {category.Title}");
         }
+        
       }
     }
   }
 }
+
