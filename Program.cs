3349f7e4b0cca79171144451101d786e429d96cc
Rickson Simoes <rickson.simoes@hotmail.com>
2022-06-29 21:36:17 -0300

    :hammer: Criando o update com dapper

--- Program.cs
+++ Program.cs
@@ -11,17 +11,17 @@ namespace DataDeposit
     {
       const string ConnectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=a1b2c3d4#@!;Trusted_Connection=False;TrustServerCertificate=True;";
 
-
       using (var connection = new SqlConnection(ConnectionString))
       {
-        ListCategories(connection);
         //CreateCategories(connection);
+        UpdateCategories(connection, new Guid("2568524C-FFF5-4F19-B83B-A2908E3E0BC4"), "AWS Cloud Services");
+        //ListCategories(connection);
       }
     }
 
     static void ListCategories(SqlConnection connection)
     {
-      var categories = connection.Query<Category>("SELECT * FROM CATEGORY");
+      var categories = connection.Query<Category>("SELECT [Id], [Title] FROM CATEGORY");
 
       foreach (var cat in categories)
       {
@@ -31,21 +31,36 @@ namespace DataDeposit
     
     static void CreateCategories(SqlConnection connection)
     {
-      var category = new Category();
-
-      category.Id = Guid.NewGuid();
-      category.Title = "Amazon AWS";
-      category.Url = "Amazon";
-      category.Summary = "AWS Cloud";
-      category.Order = 8;
-      category.Description = "Categoria destinada a serviços do AWS";
-      category.Featured = false;
+      var category = new Category()
+      {
+        Id = Guid.NewGuid(),
+        Title = "Amazon AWS",
+        Url = "Amazon",
+        Summary = "AWS Cloud",
+        Order = 8,
+        Description = "Categoria destinada a serviços do AWS",
+        Featured = false
+      };
 
       var insertSQL = @"INSERT INTO Category 
                             VALUES(@Id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
 
       var rows = connection.Execute(insertSQL, category);
 
+      Console.WriteLine($"Rows Affected: {rows}");
+    }
+
+    static void UpdateCategories(SqlConnection connection, Guid Id, string Title)
+    {
+      var category = new Category()
+      {
+        Id = Id,
+        Title = Title
+      };
+
+      var updateCategory = @"UPDATE [Category] SET [Title]=@Title WHERE [Id]=@Id";
+
+      var rows = connection.Execute(updateCategory, category);
       Console.WriteLine($"Rows Affected: {rows}");
     }
   }
